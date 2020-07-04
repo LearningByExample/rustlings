21829bc971d0803974b483be7b8fa03e4b59a8e4
Juan Medina <jamedina@gmail.com>
2020-07-04 08:02:34 +0100

    first commit

---
+++ Understanding Ownership/Move Semantics/Borrow/src/main.rs
@@ -0,0 +1,26 @@
+use std::borrow::{Borrow, BorrowMut};
+
+fn main() {
+    let vec0 = Vec::new();
+
+    let vec_aux = vec0.clone();
+
+    let mut vec1 = fill_vec(vec_aux);
+
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    vec1.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+}
+
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> /*if you are not returning a value, here should be no type*/ {
+    //let mut vec = vec.borrow();
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec /*and then you don't have to return anything here*/
+}
