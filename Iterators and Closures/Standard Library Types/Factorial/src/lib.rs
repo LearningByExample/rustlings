24d1970eedaa0d425453cba94054f4d1eabd8d34
Juan Medina <jamedina@gmail.com>
2020-07-08 08:03:51 +0100

    better factorial

--- Iterators and Closures/Standard Library Types/Factorial/src/lib.rs
+++ Iterators and Closures/Standard Library Types/Factorial/src/lib.rs
@@ -1,7 +1,3 @@
 pub fn factorial(num: u64) -> u64 {
-    if num==0 {
-        1
-    }else {
-        (1..=num).collect::<Vec<_>>().iter().product()
-    }
+    (2..=num).collect::<Vec<_>>().iter().product()
 }
