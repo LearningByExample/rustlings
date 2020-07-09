7bf052aab0b8151f9620a3c695be9df93cbbc9eb
Juan Medina <jamedina@gmail.com>
2020-07-09 07:16:40 +0100

    message passing in concurrency

--- Fearless Concurrency/Threads/Using Message Passing to Transfer Data Between Threads/src/main.rs
+++ Fearless Concurrency/Threads/Using Message Passing to Transfer Data Between Threads/src/main.rs
@@ -1,3 +1,43 @@
+use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
+
 fn main() {
-    // put you code here to launch it
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("1-hi"),
+            String::from("1-from"),
+            String::from("1-the"),
+            String::from("1-thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("2-more"),
+            String::from("2-messages"),
+            String::from("2-for"),
+            String::from("2-you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(250));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+
+
 }
